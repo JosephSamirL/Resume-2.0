360004decc4c06900791b08e8d8cd8616e53c9d1
joseph <joseph.samir@msa.edu.eg>
2023-03-09 01:45:07 +0200

    adding about

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,86 +15,74 @@ interface Props {
     }
 }
 const particlesOptions = {
-    background: {
-      color: {
-        value: "transparent",
-      },
-    },
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: false,
-          mode: "push",
-        },
-        onHover: {
-          enable: false,
-          mode: "repulse",
-        },
-        resize: true,
-      },
-      modes: {
-        bubble: {
-          distance: 400,
-          duration: 2,
-          opacity: 0.8,
-          size: 40,
-        },
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    fullScreen: {
-      enable: false,
-      zIndex: 0
-    },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: false,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: false,
-      },
-      move: {
-        direction: "top",
+  particles: {
+    number: {
+      value: 100,
+      density: {
         enable: true,
-        outMode: "out",
-        random: false,
-        speed: 3,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 30,
-      },
-      opacity: {
-        value: 0.9,
-      },
-      shape: {
-        type: "edge",
-      },
-      size: {
-        random: true,
-        value: 3,
+        value_area: 800,
       },
     },
-  detectRetina: true,
-}
+    color: {
+      value: ['#ffffff', '#ff0000', '#00ff00', '#0000ff'],
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000',
+      },
+
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 20,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 3,
+        size_min: 0,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    
+    },
+  },
+  retina_detect: true,
+};
+
 function Header(props: Props) {
     // @ts-ignore
     const particlesInit = async (main: Engine) => {
