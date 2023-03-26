285f2b59afd00e28804521655453be2172393ebf
unknown <joseph.samir@msa.edu.eg>
2023-03-26 14:11:44 +0200

    fixing ts error css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import client from '../../lib/client';
 import '@/styles/globals.css'
 import '@/styles/mediaQueries.css'
 import type { AppProps } from 'next/app'
-import "../styles/Parallax.css"
+import "@/styles/Parallax.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return  <ApolloProvider client={client}>
